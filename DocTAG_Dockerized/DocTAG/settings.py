396c43ff1d751a85e238391d5402e99a955d858f
LeonHecht <50410878+LeonHecht@users.noreply.github.com>
2025-01-27 21:12:32 -0600

    add logging to configure_data

--- DocTAG_Dockerized/DocTAG/settings.py
+++ DocTAG_Dockerized/DocTAG/settings.py
@@ -34,6 +34,21 @@ INSTALLED_APPS = [
 
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
+
 MIDDLEWARE = [
     # 'corsheaders.middleware.CorsMiddleware',
 
